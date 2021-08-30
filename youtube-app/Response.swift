d8ebdd147aa1347231106c97f3e5cd5cb0db8cd2
George <georgeo@Georges-MacBook-Air.local>
2021-08-29 23:52:38 -0400

    added code to parse json response

---
+++ youtube-app/Response.swift
@@ -0,0 +1,23 @@
+//
+//  Response.swift
+//  youtube-app
+//
+//  Created by George on 8/29/21.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    
+    var items: [Video]?
+    
+    enum CodingKeys:String, CodingKey {
+        case items
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+    }
+}
